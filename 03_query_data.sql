6aaae9bd11b234408283b3e38ddb113260a980c9
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-10-14 13:53:20 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,9 @@
-SELECT * FROM CD.FACILITIES
+-- Retrieve everything from a table
+-- How can you retrieve all the information from the cd.facilities table?
+
+SELECT * FROM cd.facilities;
+
+-- Retrieve specific columns from a table
+-- You want to print out a list of all of the facilities and their cost to members. 
+-- How would you retrieve a list of only facility names and costs?
+SELECT name, membercost FROM cd.facilities;
