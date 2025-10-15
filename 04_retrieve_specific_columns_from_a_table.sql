d041dad70c8b044f7fc7443affaa47c2415e78f6
rafaelSanchez4330 <rafaelsanchez9924@gmail.com>
2025-10-14 19:33:50 -0600

    Delete 04_retrieve_specific_columns_from_a_table.sql

--- 04_retrieve_specific_columns_from_a_table.sql
+++
@@ -1,3 +0,0 @@
---Retrieve specific columns from a table
---You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
-SELECT name, membercost FROM cd.facilities;
