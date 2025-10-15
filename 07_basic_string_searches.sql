215e111797d4a0a429d5c0b9cf3cbb016ea2e39e
Diego0086 <178218@upslp.edu.mx>
2025-10-14 19:44:23 -0600

    Update 07_basic_string_searches.sql

--- 07_basic_string_searches.sql
+++ 07_basic_string_searches.sql
@@ -1,3 +1,3 @@
 -- Basic string searches
 -- How can you produce a list of all facilities with the word 'Tennis' in their name?
-select * from cd.facilities where name like '%Tennis%'
+select * from cd.facilities where name like '%Tennis%';
