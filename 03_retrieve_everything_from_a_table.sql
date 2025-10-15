50604599419c5fa28fbc40d03a2d3da0d7b83b17
Palau003 <178561@upslp.edu.mx>
2025-10-14 19:37:06 -0600

    Update 03_retrieve_everything_from_a_table.sql

--- 03_retrieve_everything_from_a_table.sql
+++ 03_retrieve_everything_from_a_table.sql
@@ -2,15 +2,3 @@
 -- How can you retrieve all the information from the cd.facilities table?
 
 SELECT * FROM cd.facilities;
-select * from cd.facilities;
-select name, membercost from cd.facilities;    
-select * from cd.facilities where membercost > 0;   
-select facid, name, membercost, monthlymaintenance 
-	from cd.facilities 
-	where 
-		membercost > 0 and 
-		(membercost < monthlymaintenance/50.0);
-select *
-	from cd.facilities 
-	where 
-		name like '%Tennis%';  
