1625b5e5cab92031d2de854f182ae132bfaff381
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-15 22:01:18 -0600

    Update 16_work_out_the_start_times_of_bookings_for_tennis_courts.sql

--- 16_work_out_the_start_times_of_bookings_for_tennis_courts.sql
+++ 16_work_out_the_start_times_of_bookings_for_tennis_courts.sql
@@ -1,1 +1,11 @@
 --Work out the start times of bookings for tennis courts
+SELECT cd.bookings.starttime AS start, cd.facilities.name AS name
+	FROM 
+		cd.facilities
+		INNER JOIN cd.bookings
+			ON cd.facilities.facid = cd.bookings.facid
+	WHERE 
+		cd.facilities.name IN ('Tennis Court 2','Tennis Court 1') AND
+		cd.bookings.starttime >= '2012-09-21' AND
+		cd.bookings.starttime < '2012-09-22'
+ORDER BY cd.bookings.starttime;
