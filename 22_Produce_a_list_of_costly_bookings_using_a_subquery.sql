87e005ca48e15167ee69410a94e7574f15c06895
VictorCastro135 <178920@upslp.edu.mx>
2025-10-17 13:42:17 -0600

    feature/178920

---
+++ 22_Produce_a_list_of_costly_bookings_using_a_subquery.sql
@@ -0,0 +1,24 @@
+SELECT member, facility, cost FROM (
+	SELECT 
+		mems.firstname || ' ' || mems.surname AS member,
+		facs.name AS facility,
+		CASE
+			WHEN mems.memid = 0 THEN
+				bks.slots*facs.guestcost
+			ELSE
+				bks.slots*facs.membercost
+		END AS cost
+		FROM
+			cd.members mems
+			INNER JOIN cd.bookings bks
+				ON mems.memid = bks.memid
+			INNER JOIN cd.facilities facs
+				ON bks.facid = facs.facid
+		WHERE
+			bks.starttime >= '2012-09-14' AND
+			bks.starttime < '2012-09-15'
+	) AS bookings
+	WHERE cost > 30
+ORDER BY cost desc;  
+
+
