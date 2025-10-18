e6798be1229e2418f17f30d8f71aa1c4608843e6
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-17 18:44:43 -0600

    Create 42_output_the_facility_id_that_has_the_highest_number_of_slots_booked.sql

---
+++ 42_output_the_facility_id_that_has_the_highest_number_of_slots_booked.sql
@@ -0,0 +1,9 @@
+--Output the facility id that has the highest number of slots booked
+select facid, sum(slots) as totalslots
+	from cd.bookings
+	group by facid
+	having sum(slots) = (select max(sum2.totalslots) from
+		(select sum(slots) as totalslots
+		from cd.bookings
+		group by facid
+		) as sum2);
