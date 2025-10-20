48b86b5466e64080d2347ca31f1ccd1b619ee6ca
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-20 13:42:36 -0600

    Create 63_work_out_the_utilisation_percentage_for_each_facility_by_month.sql

---
+++ 63_work_out_the_utilisation_percentage_for_each_facility_by_month.sql
@@ -0,0 +1,17 @@
+--Work out the utilisation percentage for each facility by month
+--Work out the utilisation percentage for each facility by month, sorted by name and month, rounded to 1 decimal place. 
+--Opening time is 8am, closing time is 8.30pm. You can treat every month as a full month, regardless of if there were some dates the club was not open.
+
+select name, month, 
+	round((100*slots)/
+		cast(
+			25*(cast((month + interval '1 month') as date)
+			- cast (month as date)) as numeric),1) as utilisation
+	from  (
+		select facs.name as name, date_trunc('month', starttime) as month, sum(slots) as slots
+			from cd.bookings bks
+			inner join cd.facilities facs
+				on bks.facid = facs.facid
+			group by facs.facid, month
+	) as inn
+order by name, month  
