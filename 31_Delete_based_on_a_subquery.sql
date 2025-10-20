631d703d1bdba2521766b61e4083937a947502b8
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-19 21:58:14 -0600

    Create 31_Delete_based_on_a_subquery.sql

---
+++ 31_Delete_based_on_a_subquery.sql
@@ -0,0 +1,3 @@
+--Delete based on a subquery
+--In our previous exercises, we deleted a specific member who had never made a booking. How can we make that more general, to delete all members who have never made a booking?
+DELETE FROM cd.members WHERE memid NOT IN(SELECT memid FROM cd.bookings);
