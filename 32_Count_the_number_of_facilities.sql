accf9ee32a48a3cc93f5a4ffe938be53bc2fbee3
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-19 22:01:36 -0600

    Create 32_Count_the_number_of_facilities.sql

---
+++ 32_Count_the_number_of_facilities.sql
@@ -0,0 +1,3 @@
+--Count the number of facilities
+--For our first foray into aggregates, we're going to stick to something simple. We want to know how many facilities exist - simply produce a total count.
+SELECT COUNT(facid) FROM cd.facilities; 
