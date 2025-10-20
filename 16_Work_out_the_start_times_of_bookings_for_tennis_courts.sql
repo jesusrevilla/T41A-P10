08c3beadb8247fec49a380d499e771a94670b607
Uriel4443 <182829@upslp.edu.mx>
2025-10-20 12:23:49 -0600

    Update 16_Work_out_the_start_times_of_bookings_for_tennis_courts.sql

--- 16_Work_out_the_start_times_of_bookings_for_tennis_courts.sql
+++ 16_Work_out_the_start_times_of_bookings_for_tennis_courts.sql
@@ -1,1 +1,3 @@
-
+SELECT b.starttime AS start, f.name AS name FROM cd.facilities f INNER JOIN cd.bookings b
+ON f.facid = b.facid WHERE f.name IN ('Tennis Court 2','Tennis Court 1') AND b.starttime >= '2012-09-21' AND
+b.starttime < '2012-09-22'ORDER BY b.starttime;   
