2b0671b8c942cc400ba09986312813890602bee8
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 14:22:32 -0600

    Create 49_rank_members_by_(rounded)_hours_used.sql

---
+++ 49_rank_members_by_(rounded)_hours_used.sql
@@ -0,0 +1,10 @@
+--rank_members_by_(rounded)_hours_used.sql
+select firstname, surname,
+	((sum(bks.slots)+10)/20)*10 as hours,
+	rank() over (order by ((sum(bks.slots)+10)/20)*10 desc) as rank
+
+	from cd.bookings bks
+	inner join cd.members mems
+		on bks.memid = mems.memid
+	group by mems.memid
+order by rank, surname, firstname; 
