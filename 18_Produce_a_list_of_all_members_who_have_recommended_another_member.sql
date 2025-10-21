6cee79c214c47ed92eef5ea9715d3222926684af
Jpablo179804 <179804@upslp.edu.mx>
2025-10-20 18:17:23 -0600

    Create 18_Produce_a_list_of_all_members_who_have_recommended_another_member.sql

---
+++ 18_Produce_a_list_of_all_members_who_have_recommended_another_member.sql
@@ -0,0 +1,3 @@
+select distinct recs.firstname as firstname, recs.surname as surname
+	from cd.members mems inner join cd.members recs	on recs.memid = mems.recommendedby
+order by surname, firstname;   
