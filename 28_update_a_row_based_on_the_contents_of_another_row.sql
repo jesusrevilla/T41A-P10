c504a8300c1b016807181b13f6fbf6f90e363bb1
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 12:39:09 -0600

    Create 28_update_a_row_based_on_the_contents_of_another_row.sql

---
+++ 28_update_a_row_based_on_the_contents_of_another_row.sql
@@ -0,0 +1,6 @@
+--update_a_row_based_on_the_contents_of_another_row.sql
+update cd.facilities facs
+    set
+        membercost = (select membercost * 1.1 from cd.facilities where facid = 0),
+        guestcost = (select guestcost * 1.1 from cd.facilities where facid = 0)
+    where facs.facid = 1;       
