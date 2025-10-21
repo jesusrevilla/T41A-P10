a0427d43574ccfe07e2f77a4be7b702388a3cc8e
Palau003 <178561@upslp.edu.mx>
2025-10-20 18:18:07 -0600

    Create 56_generate_a_list_of_all_the_dates_in_October_2012.sql

---
+++ 56_generate_a_list_of_all_the_dates_in_October_2012.sql
@@ -0,0 +1,4 @@
+--Generate a list of all the dates in October 2012
+--Produce a list of all the dates in October 2012. They can be output as a timestamp (with time set to midnight) or a date.
+
+select generate_series(timestamp '2012/10/01',timestamp '2012/10/31','1 day');
