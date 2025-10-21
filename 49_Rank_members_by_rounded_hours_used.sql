3f77f032dbdc99d98f65bd387312f13da65e5e7a
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-21 11:53:19 -0600

    Create 49_Rank_members_by_rounded_hours_used.sql

---
+++ 49_Rank_members_by_rounded_hours_used.sql
@@ -0,0 +1,12 @@
+--Rank members by (rounded) hours used
+--Produce a list of members (including guests), along with the number of hours they've booked in facilities, rounded to the nearest ten hours. 
+--Rank them by this rounded figure, producing output of first name, surname, rounded hours, rank. Sort by rank, surname, and first name.
+select firstname, surname,
+	((sum(bks.slots)+10)/20)*10 as hours,
+	rank() over (order by ((sum(bks.slots)+10)/20)*10 desc) as rank
+
+	from cd.bookings bks
+	inner join cd.members mems
+		on bks.memid = mems.memid
+	group by mems.memid
+order by rank, surname, firstname; 
