4df94521608a7c4454f0aadfa1c7c7570011583c
Jpablo179804 <179804@upslp.edu.mx>
2025-10-20 19:51:57 -0600

    Create 67_Work_out_the_utilisation_percentage_for_each_facility_by_month.sql

---
+++ 67_Work_out_the_utilisation_percentage_for_each_facility_by_month.sql
@@ -0,0 +1,12 @@
+select name, month, 
+	round((100*slots)/
+		cast(
+			25*(cast((month + interval '1 month') as date)
+			- cast (month as date)) as numeric),1) as utilisation
+	from  (
+		select facs.name as name, date_trunc('month', starttime) as month, sum(slots) as slots
+			from cd.bookings bks
+			inner join cd.facilities facs
+				on bks.facid = facs.facid
+			group by facs.facid, month) as inn
+order by name, month    
