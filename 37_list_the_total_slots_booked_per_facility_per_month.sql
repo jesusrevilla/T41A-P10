59aa175057100f733eb425b1aa7d04aebf2db1ac
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 14:09:39 -0600

    Create 37_list_the_total_slots_booked_per_facility_per_month.sql

---
+++ 37_list_the_total_slots_booked_per_facility_per_month.sql
@@ -0,0 +1,6 @@
+--list_the_total_slots_booked_per_facility_per_month.sql
+select facid, extract(month from starttime) as month, sum(slots) as "Total Slots"
+	from cd.bookings
+	where extract(year from starttime) = 2012
+	group by facid, month
+order by facid, month;  
