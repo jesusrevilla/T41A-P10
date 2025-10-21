6821a77b07eb9c7433efbd6bb760227b513d9bb0
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 12:32:28 -0600

    Create 21_produce_a_list_of_all_members_along_with_their-recommender_using_no_joins.sql

---
+++ 21_produce_a_list_of_all_members_along_with_their-recommender_using_no_joins.sql
@@ -0,0 +1,9 @@
+--produce_a_list_of_all_members_along_with_their-recommender_using_no_joins.sql
+select distinct mems.firstname || ' ' ||  mems.surname as member,
+	(select recs.firstname || ' ' || recs.surname as recommender 
+		from cd.members recs 
+		where recs.memid = mems.recommendedby
+	)
+	from 
+		cd.members mems
+order by member; 
