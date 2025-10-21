a8b2a138f45fa7581d44731dea26294847a5ffa4
Cruz <183213@upslp.edu.mx>
2025-10-20 19:34:36 -0600

    Create 21_produce_a_list_of_all_member_along_with_their_recommender_using_no_joins.sql

---
+++ 21_produce_a_list_of_all_member_along_with_their_recommender_using_no_joins.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT m.firstname || ' ' || m.surname AS member, 
+(
+  SELECT r.firstname || ' ' || r.surname AS recommender
+  FROM cd.members r
+  WHERE r.memid = m.recommendedby
+)
+FROM cd.members m
+ORDER BY member;
