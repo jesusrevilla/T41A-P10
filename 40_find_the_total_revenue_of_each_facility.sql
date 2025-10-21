3df226973c8ad2c6307216854fb7fad8b5e33cd9
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-10-21 13:25:13 -0600

    Add SQL query to calculate total revenue by facility

---
+++ 40_find_the_total_revenue_of_each_facility.sql
@@ -0,0 +1,15 @@
+SELECT
+    f.name,
+    SUM(
+        b.slots * CASE
+            WHEN b.memid = 0 THEN f.guestcost
+            ELSE f.membercost
+        END
+    ) AS revenue
+FROM
+    cd.bookings b
+    INNER JOIN cd.facilities f ON b.facid = f.facid
+GROUP BY
+    f.name
+ORDER BY
+    revenue;
