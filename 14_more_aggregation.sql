719ac9be002a50b70272beec30708d106c0f0094
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 12:24:16 -0600

    Create 14_more_aggregation.sql

---
+++ 14_more_aggregation.sql
@@ -0,0 +1,9 @@
+--More aggregation
+
+--You'd like to get the first and last name of the last member(s) who signed up - not just the date. How can you do that?
+
+select firstname, surname, joindate
+	from cd.members
+	where joindate = 
+		(select max(joindate) 
+			from cd.members);
