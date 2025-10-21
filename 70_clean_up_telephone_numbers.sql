0763b3f3a0a04e9616e8aa74f9be48809da76d08
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-20 19:40:38 -0600

    Create 70_clean_up_telephone_numbers.sql

---
+++ 70_clean_up_telephone_numbers.sql
@@ -0,0 +1,4 @@
+--Clean up telephone numbers
+--The telephone numbers in the database are very inconsistently formatted. 
+--You'd like to print a list of member ids and numbers that have had '-','(',')', and ' ' characters removed. Order by member id.
+select memid, translate(telephone,' ()-','') from cd.members
