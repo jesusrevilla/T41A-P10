d716b5988d92e63ef09981ed876f44694916c811
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-21 12:46:54 -0600

    Update 58_Work_out_the_number_of_seconds_between_timestamps.sql

--- 58_Work_out_the_number_of_seconds_between_timestamps.sql
+++ 58_Work_out_the_number_of_seconds_between_timestamps.sql
@@ -1,3 +1,6 @@
 --Work out the number of seconds between timestamps
 --Work out the number of seconds between the timestamps '2012-08-31 01:00:00' and '2012-09-02 00:00:00'
-select extract(epoch from (timestamp '2012-09-02 00:00:00' - '2012-08-31 01:00:00'));  
+SELECT CAST(EXTRACT(EPOCH FROM (TIMESTAMP '2012-09-02 00:00:00' 
+                              - TIMESTAMP '2012-08-31 01:00:00')) AS BIGINT) 
+       AS seconds_difference;
+
