a487561fa810d6765956159af6a0182e7cfb25b8
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 12:36:01 -0600

    Create 25_insert_calculated_data_into_a_table.sql

---
+++ 25_insert_calculated_data_into_a_table.sql
@@ -0,0 +1,4 @@
+--insert_calculated_data_into_a_table.sql
+insert into cd.facilities
+    (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
+    select (select max(facid) from cd.facilities)+1, 'Spa', 20, 30, 100000, 800;
