c24647315ba63ff14ac348648dfad765a4f88dd2
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 14:12:46 -0600

    Create 40_find_the_total_revenue_of_each_facility.sql

---
+++ 40_find_the_total_revenue_of_each_facility.sql
@@ -0,0 +1,10 @@
+--find_the_total_revenue_of_each_facility.sql
+select facs.name, sum(slots * case
+			when memid = 0 then facs.guestcost
+			else facs.membercost
+		end) as revenue
+	from cd.bookings bks
+	inner join cd.facilities facs
+		on bks.facid = facs.facid
+	group by facs.name
+order by revenue;
