2eb17016b1b0d1bac77456fe1cdfc797bde4d305
menaxixi <179800@upslp.edu.mx>
2025-10-20 19:45:47 -0600

    Create 42_the_highest_number_of_slots_booked.sql

---
+++ 42_the_highest_number_of_slots_booked.sql
@@ -0,0 +1,6 @@
+--Output the facility id that has the highest number of slots booked. For bonus points, try a version without a LIMIT clause. This version will probably look messy!
+select facid, sum(slots) as "Total Slots"
+	from cd.bookings
+	group by facid
+order by sum(slots) desc
+LIMIT 1;    
