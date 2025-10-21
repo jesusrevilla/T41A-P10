095860afd023c927d111281b47784269737e1062
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-20 19:20:23 -0600

    Create SQL for case-insensitive search on facilities
    Add SQL query for case-insensitive search of facilities

---
+++ 66_perform_a_case-insensitive_search.sql
@@ -0,0 +1,4 @@
+--Perform a case-insensitive search
+--Perform a case-insensitive search to find all facilities whose name begins with 'tennis'. Retrieve all columns.
+
+select * from cd.facilities where upper(name) like 'TENNIS%'
