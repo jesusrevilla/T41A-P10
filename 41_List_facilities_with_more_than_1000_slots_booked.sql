271ca88cea69a8db8f273bdf627fc4dadb200b17
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-10-20 19:38:23 -0600

    Add files via upload

---
+++ 41_List_facilities_with_more_than_1000_slots_booked.sql
@@ -0,0 +1,8 @@
+--List facilities with more than 1000 slots booked
+--Produce a list of facilities with more than 1000 slots booked. Produce an output table consisting of facility id and slots, sorted by facility id.
+
+select facid, sum(slots) as "Total Slots"
+        from cd.bookings
+        group by facid
+        having sum(slots) > 1000
+        order by facid
---
+++ 48_Produce_a_list_of_member_names_with_each_row_containing_the_total_member_count.sql
@@ -0,0 +1,6 @@
+--Produce a list of member names, with each row containing the total member count
+--Produce a list of member names, with each row containing the total member count. Order by join date, and include guest members.
+
+select count(*) over(), firstname, surname
+	from cd.members
+order by joindate
