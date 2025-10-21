f6696e6cbb333ff52833d9f4464574c4cd36fe04
Jpablo179804 <179804@upslp.edu.mx>
2025-10-20 18:22:38 -0600

    Create 21_Produce_a_list_of_costly_bookings.sql

---
+++ 21_Produce_a_list_of_costly_bookings.sql
@@ -0,0 +1,20 @@
+select 
+    concat(mems.firstname, ' ', mems.surname) as member, 
+    facs.name as facility, 
+    (bks.slots * 
+        case 
+            when mems.memid = 0 then facs.guestcost 
+            else facs.membercost 
+        end
+    ) as cost
+from cd.members mems
+join cd.bookings bks on mems.memid = bks.memid
+join cd.facilities facs on bks.facid = facs.facid
+where 
+    bks.starttime >= '2012-09-14' and 
+    bks.starttime < '2012-09-15'
+    and (
+        (mems.memid = 0 and bks.slots * facs.guestcost > 30) or
+        (mems.memid != 0 and bks.slots * facs.membercost > 30)
+    )
+order by cost desc;
