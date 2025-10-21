f785c085bc9be6555de5437d3ded392c16d38f6e
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-10-20 19:31:28 -0600

    Add SQL query to classify facilities by revenue

---
+++ 51_classify_facilities_by_value.sql
@@ -0,0 +1,15 @@
+select name, case 
+	when class=1 then 'high'
+	when class=2 then 'average'
+	else 'low' end revenue
+	from (
+		select fac.name as name, ntile(3) over (order by sum(case
+				when memid = 0 then slots * guestcost
+				else slots * membercost
+			end) desc) as class
+		from cd.bookings book
+		inner join cd.facilities fac
+			on fac.facid = book.facid
+		group by fac.name
+	) as subquery
+order by class, name;   
