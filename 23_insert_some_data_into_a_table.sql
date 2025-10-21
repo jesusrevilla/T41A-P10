0f6b6e443b354bef02ad54467f53e8bf872f8d2f
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-10-21 10:57:34 -0600

    Insert data into facilities table

---
+++ 23_insert_some_data_into_a_table.sql
@@ -0,0 +1,16 @@
+INSERT INTO cd.facilities (
+    facid,
+    name,
+    membercost,
+    guestcost,
+    initialoutlay,
+    monthlymaintenance
+)
+VALUES (
+    9,
+    'Spa',
+    20,
+    30,
+    100000,
+    800
+);
