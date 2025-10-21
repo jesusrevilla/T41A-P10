242ac458fa4975333f9fb526fca99fb1e888be2e
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-10-20 19:37:35 -0600

    Create 21_Produce_a_list_of_costly_bookings.sql

---
+++ 21_Produce_a_list_of_costly_bookings.sql
@@ -0,0 +1,16 @@
+SELECT m.firstname || ' ' || m.surname AS member,
+       f.name AS facility,
+       CASE 
+           WHEN m.memid = 0 THEN b.slots * f.guestcost
+           ELSE b.slots * f.membercost
+       END AS cost
+FROM cd.members m
+INNER JOIN cd.bookings b ON m.memid = b.memid
+INNER JOIN cd.facilities f ON b.facid = f.facid
+WHERE b.starttime >= '2012-09-14' 
+AND b.starttime < '2012-09-15'
+AND (
+    (m.memid = 0 AND b.slots * f.guestcost > 30) OR
+    (m.memid != 0 AND b.slots * f.membercost > 30)
+)
+ORDER BY cost DESC;
