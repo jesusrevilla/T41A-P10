ae0f2a5189b6780167646f9dd118c3f49e818c23
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-21 12:29:42 -0600

    Create 18_produce_a_list_of_all_members_along_with_their_recommender.sql

---
+++ 18_produce_a_list_of_all_members_along_with_their_recommender.sql
@@ -0,0 +1,7 @@
+--Produce_a_list_of_all_members_along_with_their_recommender
+select mems.firstname as memfname, mems.surname as memsname, recs.firstname as recfname, recs.surname as recsname
+	from 
+		cd.members mems
+		left outer join cd.members recs
+			on recs.memid = mems.recommendedby
+order by memsname, memfname;
